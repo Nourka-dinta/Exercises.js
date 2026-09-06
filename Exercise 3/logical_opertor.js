230223b0d62e85b148784b6efc3b60c2f4ff2fa7
Nourka-dinta <nouradine02kahiyeh@gmail.com>
2026-09-06 21:56:12 +0300

    Submited exercise 3

---
+++ Exercise 3/logical_opertor.js
@@ -0,0 +1,15 @@
+// logical operators
+
+let isSunny = true;
+let isWeekend = true; 
+
+//  and &&
+console.log(isSunny && isWeekend);
+
+// or ||
+console.log(isSunny || isWeekend);
+
+// invert 
+console.log(!isSunny);
+
+console.log(!isWeekend);
