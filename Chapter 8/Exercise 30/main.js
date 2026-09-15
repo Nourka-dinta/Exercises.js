e583418adecefb4e0f48037ebbee4d2e997b2972
Nourka-dinta <nouradine02kahiyeh@gmail.com>
2026-09-15 19:15:12 +0300

    Submited exercise 30: Implementing Your Own Callbacks

---
+++ Chapter 8/Exercise 30/main.js
@@ -0,0 +1,26 @@
+// 
+
+function operate(a,b, callback){
+    return callback(a,b);
+} 
+
+function add(a,b){
+    return a + b ;
+}
+
+function subt(a,b){
+    return a - b;
+}
+
+function mult(a,b){
+    return a * b;
+}
+
+function div(a,b){
+    return a / b;
+}
+
+console.log("Addition ", operate(2,3, add));
+console.log("Subtration ", operate(30,10, subt));
+console.log("Multiply ", operate(9,5, mult));
+console.log("Divide ", operate(100,4, div));
