1788840be45bd52f99a1c328982bf42067b36c0f
Nourka-dinta <nouradine02kahiyeh@gmail.com>
2026-09-16 09:07:46 +0300

    Submited exercise 32: Selecting Elements

---
+++ Chapter 8/Exercise 32/main.js
@@ -0,0 +1,10 @@
+// Selecting Elements 
+
+// querySelector
+const header = document.querySelector('h1');
+
+console.log(header);
+
+// querySelectorAll
+const para = document.querySelectorAll('p');
+console.log(para);
