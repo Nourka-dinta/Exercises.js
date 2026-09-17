9ea3ad2c9b8c9e66215b3fb99215d970b37e8d71
Nourka-dinta <nouradine02kahiyeh@gmail.com>
2026-09-17 19:46:05 +0300

    Submited exercise 36: Advanced Form Validation

--- Chapter 8/Exercise 36/main.js
+++ Chapter 8/Exercise 36/main.js
@@ -5,17 +5,17 @@ const clearHistory = document.querySelector('#clearHistory');
 
 // color selected
 colorPicker.addEventListener('input', function() {
-    const colorSelected = colorPicker.value
-    colorPreview.style.backgroundColor = colorSelected
+    const colorSelected = colorPicker.value;;
+    colorPreview.style.backgroundColor = colorSelected;
     addHistory(colorSelected);
 });
 
 // history
 function addHistory(chooseColor) {
-    const li = document.createElement('li')
-    li.textContent = chooseColor
-    li.style.color = chooseColor
-    colorHistory.appendChild(li)
+    const li = document.createElement('li');
+    li.textContent = chooseColor;
+    li.style.color = chooseColor;
+    colorHistory.appendChild(li);
 }
 
 //clear history
